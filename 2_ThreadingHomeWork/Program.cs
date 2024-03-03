178cf9e3a3f4a9e89bcafa8aec747a6c0fa4a434
vitaliy1androshulik <vatalikandroshulik@gmail.com>
2024-03-03 23:50:01 +0200

    Threading!!!

---
+++ 2_ThreadingHomeWork/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.IO;
+
+namespace _2_ThreadingHomeWork
+{
+    internal class Program
+    {
+        static void Numbers()
+        {
+            Console.WriteLine("Thread started!");
+            for (int i = 0; i <= 50; i++) 
+            { 
+                Console.WriteLine(i);
+            }
+        }
+        static void Numbers2(int a, int b)
+        {
+            Console.WriteLine("Thread started!");
+            for (int i = a; i <= b; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static int[] RandomNumbers()
+        {
+            int[] a = new int[10000];
+            Random rand = new Random();
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = rand.Next(0,5000);
+            }
+            return a;
+        }
+        static void FindMax()
+        {
+            int[] a = RandomNumbers();
+            Console.WriteLine("Max number :: "+a.Max());
+        }
+        static void FindMin()
+        {
+            int[] a = RandomNumbers();
+            Console.WriteLine("Min number :: " + a.Min());
+        }
+        static void FindAverage()
+        {
+            int[] a = RandomNumbers();
+            Console.WriteLine("Average number :: " + a.Average());
+        }
+        static void Main(string[] args)
+        {
+            //N1
+            //Thread thread = new Thread(Numbers);
+            //thread.Start();
+
+            //N2
+            //int a = 0;
+            //int b = 20;
+            //Thread thread = new Thread(()=>Numbers2(a,b));
+            //thread.Start();
+
+            //N3
+            //int countOfThreads = 3;
+            //int a = 0;
+            //int b = 100;
+            //int c = 0;
+            //do
+            //{
+            //    Thread thread = new Thread(() => Numbers2(a, b));
+            //    thread.Start();
+            //    c++;
+            //} while (c<countOfThreads);
+
+            //N4 and N5
+            //string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            //StreamWriter writer = new StreamWriter("res.txt");
+            //
+            //Thread threadFindMax = new Thread(FindMax);
+            //threadFindMax.Start();
+            //writer.WriteLine(threadFindMax);
+            //
+            //Thread threadFindMin = new Thread(FindMin);
+            //threadFindMin.Start();
+            //writer.WriteLine(threadFindMin);
+            //
+            //Thread threadFindAverage = new Thread(FindAverage);
+            //threadFindAverage.Start();
+            //writer.WriteLine(threadFindAverage);
+            //
+            //string content = writer.ToString();
+            //File.WriteAllText(path, content);
+
+            //N6
+
+        }
+    }
+}
---
+++ WPF_ThreadingHomeWork/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_ThreadingHomeWork
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Thread thread;
+        private Thread threadMin;
+        private Thread threadMax;
+        private Thread threadAverage;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(TextBoxStart.Text.Length==0|| TextBoxEnd.Text.Length == 0 || TextBoxCount.Text.Length == 0)
+            {
+                MessageBox.Show("Enter information!!","Information!",MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                int a = int.Parse(TextBoxStart.Text);
+                int b = int.Parse(TextBoxEnd.Text);
+                int count = int.Parse(TextBoxCount.Text);
+                int s = 0;
+
+                do
+                {
+                    thread = new Thread(() => Numbers2(a, b));
+                    thread.Start();
+                    s++;
+                } while (s < count);
+                threadMin = new Thread(FindMin);
+                threadMin.Start();
+                threadMax = new Thread(FindMax);
+                threadMax.Start();
+                threadAverage = new Thread(FindAverage);
+                threadAverage.Start();
+            }
+            
+        }
+        private void Numbers2(int a, int b)
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                for (int i = a; i <= b; i++)
+                {
+                    LabelResult.Content += i.ToString() + " ";
+                }
+                LabelResult.Content += "\n";
+            }));    
+        }
+        private static int[] RandomNumbers()
+        {
+            int[] a = new int[10000];
+            Random rand = new Random();
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = rand.Next(0, 5000);
+            }
+            return a;
+        }
+        private void FindMax()
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                int[] a = RandomNumbers();
+                LabelMax.Content = a.Max();
+            }));
+        }
+        private void FindMin()
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                int[] a = RandomNumbers();
+                LabelMin.Content = a.Min();
+            }));
+            
+        }
+        private void FindAverage()
+        {
+            this.Dispatcher.Invoke(new Action(() =>
+            {
+                int[] a = RandomNumbers();
+                LabelAverage.Content = a.Average();
+            }));
+        }
+
+        private void BtnSaveToFile_Click(object sender, RoutedEventArgs e)
+        {
+            string path = @"C:\Users\Vitaliy\Desktop\test\Test.txt";
+            
+
+            if (File.Exists(path)) 
+            {
+                File.Delete(path);
+            }
+            else
+            {
+                FileStream fs = File.Create(path);
+                fs.Close();
+            }
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.WriteLine("Average number : "+LabelAverage.Content.ToString());
+                writer.WriteLine("Max. number : " + LabelMax.Content.ToString());
+                writer.WriteLine("Min. number : " + LabelMin.Content.ToString());
+            }
+
+        }
+    }
+}
